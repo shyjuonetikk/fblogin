f8e9018abcd1c5c8afd52a64a223defaeb8f93b8
shyjuonetikk <shyju@onetikk.com>
2019-10-14 13:39:06 +0530

    fb callback file updated

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 	$helper = $fb->getRedirectLoginHelper();
 
 	$permissions = ['email']; // Optional permissions
-	$loginUrl = $helper->getLoginUrl('http://dev.kazadee.tech/facebooklogin/fb-callback.php', $permissions);
+	$loginUrl = $helper->getLoginUrl('https://shyjufacebooklogin.herokuapp.com/fb-callback.php', $permissions);
 
 	echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 
