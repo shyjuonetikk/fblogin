30237311b22dd20c8ccb7223fe9ad2bfbca6da29
shyjuonetikk <shyju@onetikk.com>
2019-10-14 13:27:31 +0530

    Intial commit

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+
+	session_start();
+	require 'Facebook/autoload.php';
+	$fb = new Facebook\Facebook([
+	  'app_id' => '510749116427614', // Replace {app-id} with your app id
+	  'app_secret' => '308009010bfa314e2ce22bef5516a86b',
+	  'default_graph_version' => 'v3.2',
+	  ]);
+
+	$helper = $fb->getRedirectLoginHelper();
+
+	$permissions = ['email']; // Optional permissions
+	$loginUrl = $helper->getLoginUrl('http://dev.kazadee.tech/facebooklogin/fb-callback.php', $permissions);
+
+	echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+
+?>
